2ef287e8f14b45744b79ace51924ca5e8c870016
cachewraith <somonorhong011@gmail.com>
2026-04-06 22:17:03 +0700

    learn(auth-api): simple login and register user

---
+++ routes/api.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Route;
+
+use function Pest\Laravel\json;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('register',function(Request $request){
+
+    // 1. Validate
+    $request->validate([
+        'name' => 'required|string',
+        'email' => 'required|email|unique:users,email',
+        'password' => 'required|min:6'
+    ]);
+
+    // 2. password
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password)
+    ]);
+
+    return response()->json([
+        'status' => 'success',
+        'msesage' => 'Registered successfully',
+        'user' => $user
+    ]);
+});
+
+Route::post('login', function(Request $request){
+
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
+
+    if(!Auth::attempt($request->only('email','password'))) {
+        return response()->json([
+            'status' => 'failed',
+            'message' => 'Email or password invalid'
+        ],401);
+    }
+
+    $token = Auth::user()->createToken('laravel-blog-api')->plainTextToken;
+
+    return response()->json([
+        'status' => 'success',
+        'message' => 'Loggin successfully',
+        'user' => Auth::user(),
+        'token' => $token
+    ]);
+});
